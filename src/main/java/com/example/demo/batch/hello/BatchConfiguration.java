90aed60421f28ef8c006c18721916be7f4083547
ponpoko04 <taum0715@gmail.com>
2019-08-18 23:19:36 +0900

    taskletを試してみた

--- src/main/java/com/example/demo/batch/hello/BatchConfiguration.java
+++ src/main/java/com/example/demo/batch/hello/BatchConfiguration.java
@@ -2,16 +2,20 @@ package com.example.demo.batch.hello;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -59,23 +63,37 @@ public class BatchConfiguration {
 
     // tag::jobstep[]
     @Bean
-    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
+    public Job importUserJob(JobCompletionNotificationListener listener, Step step1, Step step2) {
         return jobBuilderFactory.get("importUserJob")
                 .incrementer(new RunIdIncrementer())
                 .listener(listener)
-                .flow(step1)
-                .end()
+                .start(step1)
+                .next(step2)
+//                .flow(step1)
+//                .end()
                 .build();
     }
 
     @Bean
     public Step step1(JdbcBatchItemWriter<Person> writer) {
         return stepBuilderFactory.get("step1")
-                .<Person, Person> chunk(10)
+                .<Person, Person> chunk(1)
                 .reader(reader())
                 .processor(processor())
                 .writer(writer)
                 .build();
+    }
+
+    @Bean
+    public Step step2(Tasklet tasklet) {
+        return stepBuilderFactory.get("step2")
+                .tasklet(tasklet)
+                .build();
+    }
+
+    @Bean
+    public Tasklet tasklet() {
+        return (StepContribution stepContribution, ChunkContext chunkContext) -> RepeatStatus.FINISHED;
     }
     // end::jobstep[]
 }
